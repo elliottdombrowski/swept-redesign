1d9ed979a8679e390284c436ec5260d993a2eb54
elliott dombrowski <elliottroyal@me.com>
2022-03-12 08:56:24 -0600

    tweaked values of animate component

--- client/src/AnimatePage.js
+++ client/src/AnimatePage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const animation = {
-  initial: { opacity: 0, x: 300 },
+  initial: { opacity: 0, x: 100 },
   animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: -300 },
+  exit: { opacity: 0, x: -100 },
 };
 
 const AnimatePage = ({ children }) => {
