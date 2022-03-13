fc69ae8d132a9c04d5f55fbba3c5036e62fe74aa
elliott dombrowski <elliottroyal@me.com>
2022-03-13 13:53:07 -0500

    implemented basic functionality of saved profile slider"

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME, QUERY_USER_SWEEPERS, QUERY_USER_SNOW } from '../../utils/queries';
@@ -10,6 +11,7 @@ import './query.scss';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [moveSlider, setMoveSlider] = useState(false);
 
   const { loading, data } = useQuery(QUERY_ME, {
     variables: { username: userParam },
@@ -27,8 +29,32 @@ const Profile = () => {
         </div>
 
         <section className='saved-wrapper'>
-          <SavedSweepers />
-          <SavedSnow />
+          <div className='slider-wrapper'>
+
+            <label className='switch' id='link-switcher'>
+              <input type='checkbox' />
+              <Link
+                // to='/sweeper'
+                className='slider slider-one nav-item nav-links'
+                id='sweeper-link'
+                onClick={() => setMoveSlider(false)}
+              >
+                SWEEPER
+              </Link>
+
+              <Link
+                // to='/snow'
+                className='slider slider-two nav-item nav-links'
+                id='snow-link'
+                onClick={() => setMoveSlider(true)}
+              >
+                SNOW
+              </Link>
+              <div className={moveSlider ? 'switch-overlay-right' : 'switch-overlay-left'} id='switcher' />
+            </label>
+          </div>
+          {/* <SavedSweepers />
+          <SavedSnow /> */}
         </section>
       </section>
     </main>
