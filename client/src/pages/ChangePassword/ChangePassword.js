11a542ae0741b0e01d60714a2b82913ffdf9969f
elliott dombrowski <elliottroyal@me.com>
2022-03-21 18:26:31 -0500

    set up default render for change password page

---
+++ client/src/pages/ChangePassword/ChangePassword.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const ChangePassword = () => {
+  return (
+    <main className='change-password-wrapper'>
+      changepwd
+    </main>
+  );
+};
+ 
+export default ChangePassword;
