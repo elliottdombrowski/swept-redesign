e1638784f010dbb3c8a96fee05f3bbb1a4003490
elliott dombrowski <elliottroyal@me.com>
2022-03-25 14:41:40 -0500

    .

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import SignUpButton from '../../components/SignUpButton/SignUpButton';
 import StripeCheckout from 'react-stripe-checkout';
 import { useToast } from '@chakra-ui/react';
